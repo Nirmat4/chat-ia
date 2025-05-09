a944b27330753b38b744f1150673171e8ada7d49
Nirmat4 <josafat.nova@outlook.com>
2025-05-09 14:15:25 -0600

    refactor: aside de la pagina

--- src/app/components/asidePanel.tsx
+++ src/app/components/asidePanel.tsx
@@ -1,7 +1,23 @@
+import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import AppRegistrationRoundedIcon from '@mui/icons-material/AppRegistrationRounded';
+
 export default function AsidePAnel(){
     return (
         <div className="bg-card h-full">
-
+            <div className='m-1 p-1 flex flex-row justify-between items-center'>
+                <div className='hover:bg-card w-[38px] h-[38px] rounded-[10px] flex flex-col justify-center items-center'>
+                    <DashboardRoundedIcon/>
+                </div>
+                <div className='flex flex-row justify-around items-center gap-1'>
+                    <div className='hover:bg-card w-[38px] h-[38px] rounded-[10px] flex flex-col justify-center items-center'>
+                        <SearchRoundedIcon/>
+                    </div>
+                    <div className='hover:bg-card w-[38px] h-[38px] rounded-[10px] flex flex-col justify-center items-center'>
+                        <AppRegistrationRoundedIcon/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
