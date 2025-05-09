30629dfb6ffa92f85690164aa9fd13e707859571
Nirmat4 <josafat.nova@outlook.com>
2025-05-09 12:11:34 -0600

    refactor: conjuncion de pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import HeaderModels from "./components/headerModels";
+import MessageZone from "./components/messageZone";
 import SendMessage from "./components/sendMessage";
 
 export default function Home() {
   return (
     <div>
-      <SendMessage/>
+      <HeaderModels/>
+      <div className="flex flex-col justify-center items-center">
+        <MessageZone/>
+        <SendMessage/>
+      </div>
     </div>
   );
 }
