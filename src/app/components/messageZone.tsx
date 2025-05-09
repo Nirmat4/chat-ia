d58ee6aa4ed57806f61135d2df9190125ca166db
Nirmat4 <josafat.nova@outlook.com>
2025-05-09 12:08:15 -0600

    refactor: zona de mensajes

---
+++ src/app/components/messageZone.tsx
@@ -0,0 +1,7 @@
+export default function MessageZone(){
+    return (
+        <div className="flex flex-col m-4 p-4 gap-4 w-[50%] justify-center items-center">
+            <p className="text-[27px] font-bold">¿En que puedo ayudarte?</p>
+        </div>
+    );
+}
