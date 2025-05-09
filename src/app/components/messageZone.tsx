dd6019ebd97430f02fcf10fdb520710562637e48
Nirmat4 <josafat.nova@outlook.com>
2025-05-09 13:43:13 -0600

    style: cambio sobre el tamaño

--- src/app/components/messageZone.tsx
+++ src/app/components/messageZone.tsx
@@ -1,6 +1,6 @@
 export default function MessageZone(){
     return (
-        <div className="flex flex-col m-4 p-4 gap-4 w-[90%] md:w-[50%] justify-center items-center">
+        <div className="flex flex-col m-4 p-4 gap-4 w-[90%] md:w-[70%] lg:w-[60%] justify-center items-center">
             <p className="text-[27px] font-bold">¿Con qué puedo ayudarte?</p>
         </div>
     );
