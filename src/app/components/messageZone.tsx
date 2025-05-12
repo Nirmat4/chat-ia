a7171f207080bf195999ef110fba0bac82db4188
Nirmat4 <josafat.nova@outlook.com>
2025-05-12 11:19:13 -0600

    refactor: update sizes in components

--- src/app/components/messageZone.tsx
+++ src/app/components/messageZone.tsx
@@ -6,7 +6,7 @@ export default function MessageZone(){
     console.log(messages);
 
     return (
-        <div className="flex flex-col m-4 p-4 gap-4 w-[90%] md:w-[70%] lg:w-[60%] overflow-y-auto">
+        <div className="flex flex-col m-4 p-4 gap-4 w-[90%] xl:w-[70%] overflow-y-auto">
             {messages.length<0?(
                 <div className="flex flex-col justify-center items-center">
                     <p className="text-[27px] font-bold">¿Con qué puedo ayudarte?</p>
