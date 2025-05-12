9486ac978406a7ac0ae3243bca5f90d3b1c5a381
Nirmat4 <josafat.nova@outlook.com>
2025-05-12 11:08:33 -0600

    feat: cambio en tamaños w de las distribuciones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
-import AsidePAnel from "./components/asidePanel";
+import AsidePanel from "./components/asidePanel";
 import HeaderModels from "./components/headerModels";
 import MessageZone from "./components/messageZone";
 import SendMessage from "./components/sendMessage";
 
 export default function Home() {
   return (
-    <div className="flex flex-row w-full h-screen">
-      <div className="w-[20%]">
-        <AsidePAnel/>
+    <div className="flex flex-row w-full h-screen overflow-hidden">
+      <div className="w-[260px]">
+        <AsidePanel/>
       </div>
-      <div className="flex flex-col gap-1 w-[80%]">
-        <HeaderModels/>
-        <div className="flex flex-col h-full justify-center items-center">
-          <MessageZone/>
-          <SendMessage/>
+      <div className="flex flex-col gap-1 w-full h-full">
+        <div className="h-[60px]">
+          <HeaderModels/>
+        </div>
+        <div className="flex flex-col">
+          <div className="h-[calc(100vh-60px-130px)] w-full flex flex-col justify-center items-center overflow-y-auto">
+            <MessageZone/>
+          </div>
+          <div className="h-[130px] w-full flex flex-col justify-center items-center">
+            <SendMessage/>
+          </div>
         </div>
       </div>
     </div>
