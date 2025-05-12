edd9105dbefe836770025805e39024723f588fc0
Nirmat4 <josafat.nova@outlook.com>
2025-05-12 11:30:17 -0600

    fead: new test data spaces

---
+++ src/app/database/spaces.js
@@ -0,0 +1,6 @@
+export const spaces_send=[
+    {id: 1, name: "sapace 1", description: "description 1", date: "2023-10-01",},
+    {id: 2, name: "sapace 2", description: "description 2", date: "2023-10-01",},
+    {id: 3, name: "sapace 3", description: "description 3", date: "2023-10-01",},
+    {id: 4, name: "sapace 4", description: "description 4", date: "2023-10-01",},
+]
