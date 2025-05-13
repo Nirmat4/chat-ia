d122a693ed1519f82181faca0adb12571d848a39
Nirmat4 <josafat.nova@outlook.com>
2025-05-12 19:57:57 -0600

    feat: update src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,11 @@ export default function Home() {
   return (
     <div className="flex flex-row w-full h-screen overflow-hidden">
       <ContextProvider>
-        <div className="w-[260px]">
-          <AsidePanel/>
-        </div>
-        <div className="flex flex-col gap-1 w-full h-full">
-          <div className="h-[60px]">
-            <HeaderModels/>
-          </div>
-          <div className="flex flex-col">
-            <div className="h-[calc(100vh-60px-130px)] w-full flex flex-col justify-center items-center overflow-y-auto">
-              <MessageZone/>
-            </div>
-            <div className="h-[130px] w-full flex flex-col justify-center items-center">
-              <SendMessage/>
-            </div>
-          </div>
+        <AsidePanel/>
+        <div className="flex flex-col h-screen w-full justify-start items-center">
+          <HeaderModels/>
+          <MessageZone/>
+          <SendMessage/>
         </div>
       </ContextProvider>
     </div>
