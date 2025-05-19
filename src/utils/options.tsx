b698def064d2127d8e997e82f4e49c4076c9c18c
Josafat Guerrero Flores <josafat.nova@outlook.com>
2025-05-19 09:58:28 -0600

    style: Style update in options bottles

--- src/utils/options.tsx
+++ src/utils/options.tsx
@@ -1,19 +1,23 @@
 import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
 import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
 import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
+import CachedRoundedIcon from '@mui/icons-material/CachedRounded';
 
-const style = { fontSize: 20 };
+const style = { fontSize: 17 };
 export default function Options(){
   return (
-    <div className='flex flex-row gap-1 items-center mt-2 opacity-0 hover:opacity-100 transition-opacity duration-150'>
-      <div className="p-1 px-2 rounded-lg justify-center items-center hover:bg-card hover:backdrop-blur-sm">
+    <div className='flex flex-row items-center mt-2 opacity-0 hover:opacity-100 transition-opacity duration-150'>
+      <div className="p-[2px] px-1 rounded-lg justify-center items-center hover:bg-card hover:backdrop-blur-sm">
         <ContentCopyRoundedIcon style={style}/>
       </div>
-      <div className="p-1 px-2 rounded-lg justify-center items-center hover:bg-card hover:backdrop-blur-sm">
+      <div className="p-[2px] px-1 rounded-lg justify-center items-center hover:bg-card hover:backdrop-blur-sm">
         <ThumbUpAltOutlinedIcon style={style}/>
       </div>
-      <div className="p-1 px-2 rounded-lg justify-center items-center hover:bg-card hover:backdrop-blur-sm">
+      <div className="p-[2px] px-1 rounded-lg justify-center items-center hover:bg-card hover:backdrop-blur-sm">
         <ThumbDownAltOutlinedIcon style={style}/>
+      </div>
+      <div className="p-[2px] px-1 rounded-lg justify-center items-center hover:bg-card hover:backdrop-blur-sm">
+        <CachedRoundedIcon style={style}/>
       </div>
     </div>
   )
