acc1ad104b8e591620df7b4b85d94deb1b52c6b2
Josafat Guerrero Flores <josafat.nova@outlook.com>
2025-05-19 15:58:21 -0600

    refactor: Changes in variable names from context

--- src/utils/chatTime.tsx
+++ src/utils/chatTime.tsx
@@ -2,18 +2,18 @@
 import { Context } from "@/app/context/context";
 import { useContext, useEffect, useState } from "react";
 
-interface Space{
+interface Chat{
   id: string
   name: string
   date: string
 }
 
 export default function ChatTime(){
-  const { spaces } = useContext(Context);
-  const [hoy, setHoy] = useState<Space[]>([]);
-  const [semana, setSemana] = useState<Space[]>([]);
-  const [mes, setMes] = useState<Space[]>([]);
-  const [año, setAño] = useState<Space[]>([]);
+  const { chats } = useContext(Context);
+  const [hoy, setHoy] = useState<Chat[]>([]);
+  const [semana, setSemana] = useState<Chat[]>([]);
+  const [mes, setMes] = useState<Chat[]>([]);
+  const [año, setAño] = useState<Chat[]>([]);
   useEffect(()=>{
     const hoy = new Date();
     hoy.setHours(0, 0, 0, 0);
@@ -25,54 +25,54 @@ export default function ChatTime(){
     const haceUnAnio = new Date(hoy);
     haceUnAnio.setFullYear(hoy.getFullYear() - 1);
 
-    setHoy(spaces.filter(item=>{
+    setHoy(chats.filter(item=>{
     const fechaItem = new Date(item.date);
       return fechaItem > hoy;
     }))
-    setSemana(spaces.filter(item=>{
+    setSemana(chats.filter(item=>{
       const fechaItem = new Date(item.date);
       return fechaItem > haceUnaSemana && fechaItem < hoy;
     }))
-    setMes(spaces.filter(item=>{
+    setMes(chats.filter(item=>{
       const fechaItem = new Date(item.date);
       return fechaItem > haceUnMes && fechaItem < haceUnaSemana;
     }))
-    setAño(spaces.filter(item=>{
+    setAño(chats.filter(item=>{
       const fechaItem = new Date(item.date);
       return fechaItem > haceUnAnio && fechaItem < haceUnMes;
     }))
-  }, [spaces])
+  }, [chats])
   return (
     <div className="flex flex-col mx-2">
       {hoy.length==0?<></>:(
         <div>
           <p>Hoy</p>
-          {hoy.map((space)=>(
-            <Card key={space.id} space={space}/>
+          {hoy.map((chatLocal)=>(
+            <Card key={chatLocal.id} chatLocal={chatLocal}/>
           ))}
         </div>
       )}
       {semana.length==0?<></>:(
         <div>
           <p className="mb-0.5 mt-4 font-bold tex-[15px]">Esta semana</p>
-          {semana.map((space)=>(
-            <Card key={space.id} space={space}/>
+          {semana.map((chatLocal)=>(
+            <Card key={chatLocal.id} chatLocal={chatLocal}/>
           ))}
         </div>
       )}
       {mes.length==0?<></>:(
         <div>
           <p className="mb-0.5 mt-4 font-bold tex-[15px]">Este mes</p>
-          {mes.map((space)=>(
-            <Card key={space.id} space={space}/>
+          {mes.map((chatLocal)=>(
+            <Card key={chatLocal.id} chatLocal={chatLocal}/>
           ))}
         </div>
       )}
       {año.length==0?<></>:(
         <div>
           <p className="mb-0.5 mt-4 font-bold tex-[15px]">Este año</p>
-          {año.map((space)=>(
-            <Card key={space.id} space={space}/>
+          {año.map((chatLocal)=>(
+            <Card key={chatLocal.id} chatLocal={chatLocal}/>
           ))}
         </div>
       )}
@@ -81,17 +81,17 @@ export default function ChatTime(){
 }
 
 interface CardProps {
-  space: Space;
+  chatLocal: Chat;
 }
 
-function Card({ space }: CardProps) {
+function Card({ chatLocal }: CardProps) {
   const { handleChangeChat, chat } = useContext(Context)
   return (
     <div
-      onClick={()=>handleChangeChat(space.id)}
-      className={`${chat===space.id?"bg-[#99a1af60]":"hover:bg-card"} rounded-sm flex flex-col p-1 px-2 backdrop-blur-sm m-[1px]`}
+      onClick={()=>handleChangeChat(chatLocal.id)}
+      className={`${chat===chatLocal.id?"bg-[#99a1af60]":"hover:bg-card"} rounded-sm flex flex-col p-1 px-2 backdrop-blur-sm m-[1px]`}
     >
-      <p className="font-sans truncate text-[15px]">{space.name}</p>
+      <p className="font-sans truncate text-[15px]">{chatLocal.name}</p>
     </div>
   )
 }
