cbc59d05b3c8aca5a2a9daf9ff4697f56285ae00
Josafat Guerrero Flores <josafat.nova@outlook.com>
2025-05-20 13:22:23 -0600

    style: size style update

--- src/app/components/headerModels.tsx
+++ src/app/components/headerModels.tsx
@@ -8,7 +8,7 @@ export default function HeaderModels() {
     <div className="flex flex-row w-full h-[60px] justify-between items-center">
       <CustomDropdown />
       <div className="flex flex-row items-center justify-around gap-2">
-        <div className="flex flex-row hover:bg-card backdrop-blur-sm rounded-xl justify-between gap-2 items-center-safe p-1 px-2">
+        <div className="flex flex-row bg-card backdrop-blur-sm rounded-[14px] justify-between gap-2 items-center-safe p-1 px-2 border-[0.5px] border-[#99a1af20]">
           <IosShareRoundedIcon style={{ fontSize: 17, color: "#364153" }}/>
           <p className="text-gray-700 text-[15px] font-sans">Compartir</p>
         </div>
