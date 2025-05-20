71c7051ae6e8714f4127e4a512b07bd0bd8f12e5
Josafat Guerrero Flores <josafat.nova@outlook.com>
2025-05-20 13:19:34 -0600

    feat: Size edition during writing

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -5,28 +5,39 @@ import { Context } from "@/app/context/context";
 import { useParams } from "next/navigation";
 
 export default function MessageZone() {
-  const { messages } = useContext(Context);
-  const params = useParams();
-  const { id } = params;
+  const { messages, height } = useContext(Context);
+  const { id } = useParams();
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const el = containerRef.current;
     if (el) {
-      el.scrollTo({
-        top: el.scrollHeight,
-        behavior: "smooth",
-      });
+      el.scrollTo({ top: el.scrollHeight, behavior: "smooth" });
     }
   }, [messages]);
+
   return (
-    <div ref={containerRef} className="flex flex-col justify-start items-center m-4 p-4 gap-4 h-[calc(100vh-260px)] w-full overflow-auto scroll-smooth focus:outline-none">
+    <div
+      ref={containerRef}
+      className={`
+        flex flex-col
+        justify-start items-center
+        mb-2 gap-4 w-full
+        min-h-0
+        overflow-y-auto scroll-smooth
+        focus:outline-none
+      `}
+      style={{
+        /* altura dinámica: viewport menos tu variable `height` */
+        height: `calc(100vh - 160px - ${height}px)`,
+      }}
+    >
       {messages.length === 0 ? (
         <div className="flex flex-col justify-center items-center">
           <p className="text-[27px] font-bold">¿Con qué puedo ayudarte?</p>
         </div>
       ) : (
-        <div className="flex-1 flex flex-col w-[95%] xl:w-[75%]">
+        <div className="flex-1 flex flex-col w-full">
           {messages.map((message) => (
             <MemoizedMessageBubble key={message.id} message={message} />
           ))}
