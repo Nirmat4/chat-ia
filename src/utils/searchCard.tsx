037eb82be61e64e2ce6263a9ba0111afbd415fa9
Josafat Guerrero Flores <josafat.nova@outlook.com>
2025-05-21 13:28:44 -0600

    style: padding style update

--- src/utils/searchCard.tsx
+++ src/utils/searchCard.tsx
@@ -1,14 +1,14 @@
 export default function SearchCard() {
   return (
-    <div className="flex flex-row items-center gap-4 bg-card p-2 rounded-[18px] text-gray-700 backdrop-blur-sm border-[0.5px] border-[#99a1af10]">
-      <div className="blur-sm pl-4">
+    <div className="flex flex-row items-center gap-3 bg-card p-1 rounded-[18px] text-gray-700 backdrop-blur-sm border-[0.5px] border-[#99a1af10]">
+      <div className="blur-sm pl-2">
         <div className="p-2 animate-spin bg-gradient-to-r from-violet-500 via-blue-500 to-cyan-400 w-[50px] h-[50px] aspect-square rounded-full">
           <div className="rounded-full h-full w-full bg-[#ffffff]"></div>
         </div>
       </div>
-      <div className="p-2">
-        <p className="text-[#00000090] font-black text-lg">Busqueda Profunda</p>
-        <p className="text-[#00000080] font-sans">Estamos revisando cuidadosamente tus fuentes para encontrar justo lo que necesitas. Este proceso será breve.</p>
+      <div className="p-1">
+        <p className="text-[#00000090] font-black text-[16px]">Busqueda Profunda</p>
+        <p className="text-[#00000080] font-sans text-[14px]">Estamos revisando cuidadosamente tus fuentes para encontrar justo lo que necesitas. Este proceso será breve.</p>
       </div>
     </div>
   );
