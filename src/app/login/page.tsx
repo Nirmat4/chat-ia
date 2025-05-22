0a3883e227e165ab4a7b32d24b2fce55d2a4523f
Josafat Guerrero Flores <josafat.nova@outlook.com>
2025-05-22 14:55:29 -0600

    refactor: new login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+export default function LoginPage() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center">
+      <div className="flex flex-row justify-around items-center">
+        <div>
+          <p>Chat Bajaware</p>
+          <p>
+            Consultas SQL, conocimiento vectorial y JIRA, todo en una sola
+            conversación.
+            <br />
+            Preguntas tú, conecto yo, responde la inteligencia.
+          </p>
+        </div>
+        <div>formulario</div>
+      </div>
+    </div>
+  );
+}
