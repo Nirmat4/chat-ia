3f3d8eccbead49875ba1848fbc023cc403740f24
Josafat Guerrero Flores <josafat.nova@outlook.com>
2025-05-22 18:21:49 -0600

    fix: update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,13 @@
+import React from "react";
+import DescripstionCard from "./components/descriptionCard";
+import FormCard from "./components/formCard";
+
 export default function LoginPage() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center">
-      <div className="flex flex-row justify-around items-center">
-        <div>
-          <p>Chat Bajaware</p>
-          <p>
-            Consultas SQL, conocimiento vectorial y JIRA, todo en una sola
-            conversación.
-            <br />
-            Preguntas tú, conecto yo, responde la inteligencia.
-          </p>
-        </div>
-        <div>formulario</div>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="flex flex-row w-fit gap-[100px] p-10 rpund items-center">
+        <DescripstionCard/>
+        <FormCard/>
       </div>
     </div>
   );
